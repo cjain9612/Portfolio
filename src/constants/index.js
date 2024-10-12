d3861ce950c82913854fa0384d9f357742842850
Chirag Gandhi <cjain@Chirags-MacBook-Air.local>
2024-10-13 02:17:26 +0530

    Update

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,39 +3,25 @@ import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+export const HERO_CONTENT = `I am a dedicated full-stack developer with a strong focus on building robust and scalable web applications. With over four years of hands-on experience, I have refined my expertise in front-end frameworks such as React and Next.js, alongside back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My mission is to leverage these skills to create innovative solutions that propel business growth while delivering seamless and impactful user experiences`;
 
-export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+export const ABOUT_TEXT = `I am a versatile and driven full-stack developer with a passion for crafting efficient, user-focused web applications. Over the past five years, I’ve gained extensive experience with technologies like React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey into web development stemmed from a deep curiosity about how things work, evolving into a career fueled by constant learning and a desire to tackle new challenges. I thrive in collaborative settings, where solving complex problems and delivering high-quality solutions is key. Beyond coding, I enjoy staying active, exploring emerging technologies, and contributing to open-source projects`;
 
 export const EXPERIENCES = [
   {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
+    year: "2022 - Present",
+    role: "Associate Consultant",
+    company: "Infosys Ltd.",
     description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
     technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
   },
   {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
+    year: "2020 - 2022",
+    role: "Software Developer",
+    company: "Thinkzy Tech",
     description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
+    technologies: ["HTML", "CSS", "Javascript", "React.js", "Node"],
+  }
 ];
 
 export const PROJECTS = [
@@ -70,7 +56,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Spice Garden, Bangalore ",
+  phoneNo: "+91 8107388307",
+  email: "cjain9612@gmail.com",
 };
